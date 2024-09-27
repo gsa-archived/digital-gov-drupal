de583badf93f4ea207bd2a5fcc7678615f64d950
Matt Poole <mattsqd@users.noreply.github.com>
2024-09-27 15:12:22 -0400

    Allow default content to be exported and updated contributing documentation.

--- RoboFile.php
+++ RoboFile.php
@@ -1,5 +1,6 @@
 <?php
 
+use Robo\ResultData;
 use Robo\Tasks;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -29,5 +30,45 @@ class RoboFile extends Tasks
         $io = new SymfonyStyle($input, $output);
         $io->comment('This is just a placeholder command, please add your own custom commands here. Please edit : ' . __FILE__);
     }
+
+    /**
+     * Export default content.
+     *
+     * @command drupal-project:export-content
+     *
+     * @return \Robo\ResultData
+     *
+     * @throws \Exception
+     */
+    public function exportContent(
+        InputInterface $input,
+        OutputInterface $output,
+        array $opts = [
+            'path' => 'modules/custom/default_content_config',
+            'entities' => [
+                'node',
+                'menu_link_content',
+                'media',
+            ],
+        ]
+    ): ResultData
+    {
+
+        $path =  $opts['path'];
+        $entities = $opts['entities'];
+        $io = new SymfonyStyle($input, $output);
+        if (is_dir("web/$path/content")) {
+            $io->info('Removing existing default content exported.');
+            $this->_cleanDir("web/$path/content");
+        }
+        foreach ($entities as $entity) {
+            $io->info("Exporting $entity as default content");
+            $this->_exec("./drush.sh default-content:export-references $entity --folder=$path/content");
+        }
+        $io->info("Finished exporting default content to $path/content");
+
+        return new ResultData();
+    }
+
 
 }
