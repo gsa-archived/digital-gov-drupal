c02a1f29603ea8508a024effd7ca960742a386a5
Matt Poole <mattsqd@users.noreply.github.com>
2024-10-02 13:01:31 -0400

    Initialized a new Drupal theme with the CSS, JS, and scripts from the previous site.

---
+++ web/themes/custom/digital_gov/gulpfile.js
@@ -0,0 +1,28 @@
+// Import Gulp 5
+const { parallel, series, watch } = require("gulp");
+
+const scripts = require("./gulp-includes/gulp/scripts");
+const styles = require("./gulp-includes/gulp/styles");
+
+function gulpWatch() {
+  const THEME_DIR = "./";
+  watch(`${THEME_DIR}/scss/uswds/**/*.scss`, styles.buildSass);
+  watch(`${THEME_DIR}/scss/new/**/*.scss`, styles.buildSass);
+  watch(`${THEME_DIR}/js/*.js`, scripts.compile);
+}
+
+// Define public tasks
+exports.copyUswdsJS = scripts.copyUswdsJS;
+exports.copyUswdsImages = styles.copyUswdsImages;
+exports.copyUswdsFonts = styles.copyUswdsFonts;
+exports.buildSprite = styles.buildSprite;
+exports.copyUswdsAssets = parallel(
+  styles.copyUswdsImages,
+  styles.copyUswdsFonts
+);
+exports.buildAssets = parallel(styles.buildSass, scripts.compile);
+exports.buildSass = styles.buildSass;
+exports.buildJS = scripts.compile;
+exports.watch = gulpWatch;
+//exports.default = series(styles.buildSass, gulpWatch);
+exports.default = series(exports.copyUswdsAssets, exports.buildAssets)
