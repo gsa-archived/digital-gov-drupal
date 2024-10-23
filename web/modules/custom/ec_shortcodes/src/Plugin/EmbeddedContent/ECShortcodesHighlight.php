d17c4fe705b16520503f3af15031ff6bfaa8c315
christian medders <christian.medders@gsa.gov>
2024-10-23 09:58:51 -0400

    adding highlight shortcode

---
+++ web/modules/custom/ec_shortcodes/src/Plugin/EmbeddedContent/ECShortcodesHighlight.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\ec_shortcodes\Plugin\EmbeddedContent;
+
+use Drupal\embedded_content\EmbeddedContentInterface;
+use Drupal\embedded_content\EmbeddedContentPluginBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Plugin iframes.
+ *
+ * @EmbeddedContent(
+ *   id = "ec_shortcodes_highlight",
+ *   label = @Translation("Highlight"),
+ *   description = @Translation("Highlights a content string."),
+ * )
+ */
+class ECShortcodesHighlight extends EmbeddedContentPluginBase implements EmbeddedContentInterface
+{
+
+    use StringTranslationTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function defaultConfiguration()
+    {
+        return [
+        'text' => NULL,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(): array
+    {
+        return [
+        '#theme' => 'ec_shortcodes_highlight',
+        '#text' => $this->configuration['text'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+    {
+        $form['text'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Text'),
+        '#default_value' => $this->configuration['text'],
+        '#required' => TRUE,
+        ];
+        return $form;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isInline(): bool
+    {
+        return TRUE;
+    }
+
+}
