7fd4967b9e7f20e6e8660ca4e1dd20385d598844
christian medders <christian.medders@gsa.gov>
2024-11-03 21:22:08 -0500

    DIGITAL-38: saving work for author bio

--- web/modules/custom/ec_shortcodes/src/Plugin/EmbeddedContent/ECShortcodesAuthorBio.php
+++ web/modules/custom/ec_shortcodes/src/Plugin/EmbeddedContent/ECShortcodesAuthorBio.php
@@ -36,8 +36,8 @@ class ECShortcodesAuthorBio extends EmbeddedContentPluginBase implements Embedde
   public function build(): array {
     return [
       '#theme' => 'ec_shortcodes_author_bio',
-      '#name' => $this->configuration['name'],
-      '#bio' => $this->configuration['bio'],
+      // '#name' => $this->configuration['name'],
+      // '#bio' => $this->configuration['bio'],
     ];
   }
 
@@ -45,18 +45,61 @@ class ECShortcodesAuthorBio extends EmbeddedContentPluginBase implements Embedde
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $form['name'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Name'),
-      '#default_value' => $this->configuration['name'],
-      '#required' => TRUE,
-    ];
-    $form['bio'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Bio'),
-      '#default_value' => $this->configuration['bio'],
-      '#required' => TRUE,
-    ];
+    // $form['name'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Name'),
+    //   '#default_value' => $this->configuration['name'],
+    //   '#required' => TRUE,
+    // ];
+    // $form['bio'] = [
+    //   '#type' => 'textfield',
+    //   '#title' => $this->t('Bio'),
+    //   '#default_value' => $this->configuration['bio'],
+    //   '#required' => TRUE,
+    // ];
+
+  //   $form['author'] = [
+  //       '#type' => 'entity_autocomplete',
+  //       '#target_type' => 'node',
+  //       '#tags' => TRUE,
+  //       '#default_value' => $node,
+  //       '#selection_handler' => 'default',
+  //       '#selection_settings' => [
+  //         'target_bundles' => ['authors'],
+  //         ],
+  //       '#autocreate' => [
+  //         'bundle' => 'article',
+  //         // 'uid' => <a valid user ID>,
+  // ],
+// ];
+
+$form['author'] = [
+  '#type' => 'entity_autocomplete',
+  '#target_type' => 'node',
+  '#tags' => TRUE,
+  '#default_value' => $node,
+  '#selection_handler' => 'default',
+  '#selection_settings' => [
+    'target_bundles' => ['authors'],
+  ],
+  '#autocreate' => [
+    'bundle' => 'article',
+    // 'uid' => <a valid user ID>,
+  ],
+  '#process' => function ($element, &$form_state, $form) {
+    $node = \Drupal::entityTypeManager()->getStorage('node')->load($element['#value']);
+    if ($node) {
+      $element['#value'] = [
+        'nid' => $node->id(),
+        'field_bio' => $node->field_bio->value,
+        'field_first_name' => $node->field_first_name->value,
+        'field_last_name' => $node->field_last_name->value,
+      ];
+    }
+    return $element;
+  },
+];
+
     return $form;
   }
 
