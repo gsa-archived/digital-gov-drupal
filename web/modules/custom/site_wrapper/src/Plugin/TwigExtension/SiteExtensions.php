14c402f056b1d5e11028dfb56f09ff1d5bb114d5
Oscar Merida <oscar.merida@gsa.gov>
2024-12-02 12:11:03 -0500

    DIGITAL-37: Display featured resources and links on topics page.
    Adds a twig function to get the favicon URI via Google.

--- web/modules/custom/site_wrapper/src/Plugin/TwigExtension/SiteExtensions.php
+++ web/modules/custom/site_wrapper/src/Plugin/TwigExtension/SiteExtensions.php
@@ -21,11 +21,14 @@ class SiteExtensions extends AbstractExtension {
   public function getFunctions() {
     return [
       new TwigFunction('dg_logo_source', [$this, 'getLogoSource']),
+      new TwigFunction('dg_lookup_favicon', [$this, 'lookupFavicon']),
     ];
   }
 
   /**
    * Returns the logo image to use for a given URL's host/domain.
+   *
+   * This method falls back to a local logo for digital.gov domains.
    *
    * USAGE:
    *    {{ dl_logo_url(my_url, '/' ~ active_theme_path()) }
@@ -33,17 +36,32 @@ class SiteExtensions extends AbstractExtension {
   public function getLogoSource(string $uri, string $active_theme_path): string {
     $host = parse_url($uri, PHP_URL_HOST);
 
-    if (!$host) {
+    if (!$host || $host === 'digital.gov' || str_contains($host, '.digital.gov')) {
       // Assume a local URL.
       return $active_theme_path . '/static/digitalgov/img/digit-50.png';
     }
 
+    return self::buildFaviconSearch($uri);
+  }
+
+  /**
+   * Ask google for the favicon to use for a given URL.
+   *
+   * USAGE:
+   *    {{ lookupFavicon(my_url) }}
+   */
+  public function lookupFavicon(string $uri, string $active_theme_path): string {
+    return self::buildFaviconSearch($uri);
+  }
+
+  /**
+   * Builds the Google favicon search URI.
+   */
+  protected static function buildFaviconSearch(string $uri): string {
+    $host = parse_url($uri, PHP_URL_HOST);
+
     // This is not a foolproof way to get domains.
     $domain = preg_replace('@^(?:.+?\\.)+(.+?\\.(?:co\\.uk|com|net|org|gov|mil|io))@', '$1', $host);
-
-    if ($domain === 'digital.gov') {
-      return $active_theme_path . '/digitalgov/img/digit-50.png';
-    }
 
     return 'https://www.google.com/s2/favicons?domain=' . $domain;
   }
