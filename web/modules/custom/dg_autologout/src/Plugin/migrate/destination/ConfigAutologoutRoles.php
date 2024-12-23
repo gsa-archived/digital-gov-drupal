daf7ce8a2a618d99f9384b41fb9f5879aa7034fc
christian medders <christian.medders@gsa.gov>
2024-12-23 13:41:38 -0500

    DIGITAL-239: Another update to code standard

--- web/modules/custom/dg_autologout/src/Plugin/migrate/destination/ConfigAutologoutRoles.php
+++ web/modules/custom/dg_autologout/src/Plugin/migrate/destination/ConfigAutologoutRoles.php
@@ -2,9 +2,10 @@
 
 namespace Drupal\dg_autologout\Plugin\migrate\destination;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\migrate\Plugin\migrate\destination\Config;
 use Drupal\migrate\Row;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Autologout Configuration Migration.
@@ -16,14 +17,50 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 class ConfigAutologoutRoles extends Config {
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new ConfigAutologoutRoles object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function import(Row $row, array $old_destination_id_values = []) {
     $dg_autologout_role = 'dg_autologout.role.';
-    $roles = entityTypeManager()->getStorage('user_role')->loadMultiple();
+    $roles = $this->entityTypeManager->getStorage('user_role')->loadMultiple();
     foreach ($roles as $role) {
       if (strtolower($row->getSourceProperty('role')) === strtolower($role->label())) {
-        $dg_autologout_role = 'dg_autologout.role.' . $role->id();
+        $dg_autologout_role = 'dg_autologout.role.'. $role->id();
         $this->config->setName($dg_autologout_role);
         $this->config->save();
         break;
