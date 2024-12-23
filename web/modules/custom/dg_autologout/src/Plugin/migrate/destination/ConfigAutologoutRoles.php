24d18d8123857f90207a5e9ca544451373f1740c
christian medders <christian.medders@gsa.gov>
2024-12-23 13:32:11 -0500

    DIGITAL-239: coding standard fix

--- web/modules/custom/dg_autologout/src/Plugin/migrate/destination/ConfigAutologoutRoles.php
+++ web/modules/custom/dg_autologout/src/Plugin/migrate/destination/ConfigAutologoutRoles.php
@@ -5,6 +5,7 @@ namespace Drupal\dg_autologout\Plugin\migrate\destination;
 use Drupal\migrate\Plugin\migrate\destination\Config;
 use Drupal\migrate\Row;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Autologout Configuration Migration.
@@ -16,11 +17,37 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 class ConfigAutologoutRoles extends Config {
 
   /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a ConfigAutologoutRoles object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function import(Row $row, array $old_destination_id_values = []) {
     $dg_autologout_role = 'dg_autologout.role.';
-    $roles = \Drupal::entityTypeManager()->getStorage('user_role')->loadMultiple();
+    $roles = $this->entityTypeManager->getStorage('user_role')->loadMultiple();
     foreach ($roles as $role) {
       if (strtolower($row->getSourceProperty('role')) === strtolower($role->label())) {
         $dg_autologout_role = 'dg_autologout.role.' . $role->id();
@@ -35,5 +62,4 @@ class ConfigAutologoutRoles extends Config {
 
     return $entity_ids;
   }
-
 }
