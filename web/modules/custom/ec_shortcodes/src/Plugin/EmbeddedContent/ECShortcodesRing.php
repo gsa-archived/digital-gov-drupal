25b2c03e70e4666855e8230f102deeac7e86338e
christian medders <christian.medders@gsa.gov>
2025-01-10 09:31:28 -0500

    DIGITAL-232: add first pass of ring short code

---
+++ web/modules/custom/ec_shortcodes/src/Plugin/EmbeddedContent/ECShortcodesRing.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\ec_shortcodes\Plugin\EmbeddedContent;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\embedded_content\EmbeddedContentInterface;
+use Drupal\embedded_content\EmbeddedContentPluginBase;
+
+/**
+ * Plugin iframes.
+ *
+ * @EmbeddedContent(
+ *   id = "ec_shortcodes_ring",
+ *   label = @Translation("Ring"),
+ *   description = @Translation("Renders a styled component."),
+ * )
+ */
+class ECShortcodesRing extends EmbeddedContentPluginBase implements EmbeddedContentInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'heading' => NULL,
+      'text' => NULL,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    return [
+      '#theme' => 'ec_shortcodes_ring',
+      '#heading' => $this->configuration['heading'],
+      '#text' => $this->configuration['text'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['heading'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Ring Component Heading'),
+      '#default_value' => $this->configuration['heading'],
+      '#required' => TRUE,
+    ];
+    $form['text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Ring Component Text'),
+      '#default_value' => $this->configuration['text'],
+      '#required' => TRUE,
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function isInline(): bool {
+    return FALSE;
+  }
+
+}
