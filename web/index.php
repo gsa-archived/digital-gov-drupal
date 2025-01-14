a0509696b20e38063c8943541e81a203321d9dca
Matt Poole <mattsqd@users.noreply.github.com>
2025-01-14 10:08:28 -0500

    DIGITAL-000: Put back conf as was and index.php to normal to see if watchdog works.

--- web/index.php
+++ web/index.php
@@ -1,12 +1,22 @@
 <?php
-$stdout = fopen('php://stdout', 'w');
-fwrite($stdout, "This should be writing to to STDOUT update 1!!!\n");
-fclose($stdout);
-?>
-<html>
-<head><title>Hello World update 1!</title></head>
-<body>
-<h1>STDOUT Test</h1>
-<p>Should have written to STDOUT</p>
-</body>
-</html>
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
