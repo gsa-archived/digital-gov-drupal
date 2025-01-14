33d528c2362df9e3d102b6086eb95dc80043cab1
Matt Poole <mattsqd@users.noreply.github.com>
2025-01-14 09:49:45 -0500

    DIGITAL-000: Update conf to disable to see if that is really required.

--- web/index.php
+++ web/index.php
@@ -1,10 +1,10 @@
 <?php
 $stdout = fopen('php://stdout', 'w');
-fwrite($stdout, "This should be writing to to STDOUT!!!\n");
+fwrite($stdout, "This should be writing to to STDOUT update 1!!!\n");
 fclose($stdout);
 ?>
 <html>
-<head><title>Hello World!</title></head>
+<head><title>Hello World update 1!</title></head>
 <body>
 <h1>STDOUT Test</h1>
 <p>Should have written to STDOUT</p>
