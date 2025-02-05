10442650420ec4998fdd88a01660f24b084fe60b
Oscar Merida <oscar.merida@gsa.gov>
2025-02-05 12:31:53 -0500

    DIGITAL-153: Address PR Feedback
    Handle mailto and domain-only URLs gracefully.
    Follow redirects and use the final destination if its valid
    Also strip the hostname for digital.gov links.

--- web/modules/custom/convert_text/src/ConvertText.php
+++ web/modules/custom/convert_text/src/ConvertText.php
@@ -52,18 +52,28 @@ class ConvertText {
    * Update local link tags with linkit data attributes.
    */
   protected static function addLinkItMarkup(string $source_text): string {
+
+    // Consider these domains local.
+    $base_domains = [\Drupal::request()->getHost(), 'digital.gov', 'www.digital.gov'];
+
     $dom = new \DOMDocument();
     $dom->loadHTML($source_text);
 
     foreach ($dom->getElementsByTagName('a') as $link) {
       $href = $link->getAttribute('href');
-      if (!$href) {
+      if (!$href || str_starts_with($href, 'mailto:')) {
         continue;
       }
 
-      $host = parse_url($href, PHP_URL_HOST) ?? '';
+      // Add a trailing slash for links with just the domain w/o trailing slash.
+      if (preg_match('/^https?:\/\/(' . implode('|', $base_domains) . ')$/', $href, $matches)) {
+        $href = $matches[0] . '/';
+      }
+      // Now, strip the local domains from links that include them.
+      $href = preg_replace('/^https?:\/\/(' . implode('|', $base_domains) . ')/', '', $href);
 
-      if (in_array($host, ['', 'digital.gov', 'www.digital.gov'])) {
+      $host = parse_url($href, PHP_URL_HOST) ?? '';
+      if ($host === '') {
         $alias = parse_url($href, PHP_URL_PATH);
 
         $sysPath = \Drupal::service('path_alias.manager')->getPathByAlias($alias);
@@ -78,6 +88,49 @@ class ConvertText {
         }
 
         $url = Url::fromUserInput($sysPath);
+
+        if (!$url->isRouted()) {
+          // What do we have here?
+          $client = \Drupal::httpClient();
+
+          $host = \Drupal::request()->getSchemeAndHttpHost();
+          $response = $client->get($host . $sysPath, [
+            // Don't throw exceptions on error codes.
+            'http_errors' => FALSE,
+            'allow_redirects' => [
+              'max' => 10,
+              'track_redirects' => TRUE,
+            ],
+          ]);
+
+          if ($response->getStatusCode() > 400) {
+            continue;
+          }
+          if ($response->getStatusCode() === 200) {
+            if ($history = $response->getHeader('X-Guzzle-Redirect-History')) {
+              $finalURI = $history[array_key_last($history)];
+              $redirHost = parse_url($finalURI, PHP_URL_HOST);
+              if (!str_ends_with($host, $redirHost)) {
+                // We were redirected off site. Let's fix the link and move on.
+                $link->setAttribute('href', $finalURI);
+                continue;
+              }
+              $redirPath = parse_url($finalURI, PHP_URL_PATH) ?? '/';
+              $url = Url::fromUserInput($redirPath);
+              if (!$url->isRouted()) {
+                // Redirected to an unrouted path, not much we can do here.
+                continue;
+              }
+              // Replace the alias in the href with the internal path.
+              $sysPath = \Drupal::request()->getBasePath() . '/' . $url->getInternalPath();
+            }
+          }
+          else {
+            // Leave 40x links alone.
+            continue;
+          }
+        }
+
         switch ($url->getRouteName()) {
           case 'entity.node.canonical':
             $params = $url->getRouteParameters();
@@ -100,8 +153,9 @@ class ConvertText {
             break;
 
           case '<front>':
-            // If someone links to the home page,
-            // we don't need to modify the link.
+            // Ensure the link to the homepage doesn't have a domain name.
+            // Don't need to add any other attributes.
+            $link->setAttribute('href', $href);
             continue 2;
 
           default:
@@ -109,6 +163,7 @@ class ConvertText {
             continue 2;
         }
 
+        // Linkit saves the internal path and renders the alias.
         $link->setAttribute('href', $sysPath);
         $link->setAttribute('data-entity-type', $entityType);
         $link->setAttribute('data-entity-uuid', $uuid);
