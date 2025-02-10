e79b6569992de6c55cb13ce99b0a7d5fc19f59cf
Oscar Merida <oscar.merida@gsa.gov>
2025-02-10 16:47:36 -0500

    DIGITAL-242: Use dependency injection to access the alias manager.

--- web/modules/custom/ec_shortcodes/src/Plugin/EmbeddedContent/ECShortcodesButton.php
+++ web/modules/custom/ec_shortcodes/src/Plugin/EmbeddedContent/ECShortcodesButton.php
@@ -3,9 +3,12 @@
 namespace Drupal\ec_shortcodes\Plugin\EmbeddedContent;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\embedded_content\EmbeddedContentInterface;
 use Drupal\embedded_content\EmbeddedContentPluginBase;
+use Drupal\path_alias\AliasManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin iframes.
@@ -16,9 +19,29 @@ use Drupal\embedded_content\EmbeddedContentPluginBase;
  *   description = @Translation("Renders a button."),
  * )
  */
-class ECShortcodesButton extends EmbeddedContentPluginBase implements EmbeddedContentInterface {
+class ECShortcodesButton extends EmbeddedContentPluginBase implements EmbeddedContentInterface, ContainerFactoryPluginInterface {
 
   use StringTranslationTrait;
+
+  public function __construct(private AliasManagerInterface $aliasManager, array $configuration, string $plugin_id, array $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * Dependency Injection via Container.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   Services container.
+   * @param array $configuration
+   *   Plugin configuration.
+   * @param string $plugin_id
+   *   Plugin string identifier.
+   * @param array $plugin_definition
+   *   Plugin definition.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self($container->get('path_alias.manager'), $configuration, $plugin_id, $plugin_definition);
+  }
 
   /**
    * {@inheritdoc}
@@ -37,7 +60,7 @@ class ECShortcodesButton extends EmbeddedContentPluginBase implements EmbeddedCo
     $url = $this->configuration['url'];
     // A URL starting with two slashes is a protocol relative external link.
     if (str_starts_with($url, '/') && !str_starts_with($url, '//')) {
-      $url = \Drupal::service('path_alias.manager')->getAliasByPath($url);
+      $url = $this->aliasManager->getAliasByPath($url);
     }
 
     return [
