0cfb3e6d575b5f7ab5fe9842330ebf2a4c1f9903
Matt Poole <mattsqd@users.noreply.github.com>
2025-03-12 11:29:21 -0400

    DIGITAL-456: Set STATIC_URI and DRUSH_OPTIONS_URI inside cloud.gov.

--- scripts/pipeline/cloud-gov-deploy.sh
+++ scripts/pipeline/cloud-gov-deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 mv manifest.yml manifest.tmp
+
+# Convert to uppercase
+CF_SPACE_UPPER=${CF_SPACE^^}
+
+DRUSH_OPTIONS_URI_VAR_NAME="${CF_SPACE_UPPER}_CMS_URL"
+export DRUSH_OPTIONS_URI="${!DRUSH_OPTIONS_URI_VAR_NAME}"
+
+STATIC_URI_VAR_NAME="${CF_SPACE_UPPER}_STATIC_URL"
+export STATIC_URI="${!DRUSH_OPTIONS_URI_VAR_NAME}"
+
 envsubst < manifest.tmp > manifest.yml
 
 cf push --strategy rolling
