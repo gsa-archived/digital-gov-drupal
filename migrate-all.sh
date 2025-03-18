28e44abd84db58e80247cf662685545dbf5a068c
Matt Poole <mattsqd@users.noreply.github.com>
2025-03-18 11:43:19 -0400

    DIGITAL-467: Added removed content command to migrate-all.

--- migrate-all.sh
+++ migrate-all.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # Runs all the migration steps in the expected order
-# 1. Build the feed for s3files that are directly linked in markdown,
+
+# 1. Remove content created by default content that will be imported by the
+# scripts.
+./drush.sh rcbm
+
+# 2. Build the feed for s3files that are directly linked in markdown,
 #    not with the asset short code
 ./drush.sh digitalgov:s3feed > web/sites/default/files/s3files.json
 
-# 2. Run all the migrations.
+# 3. Run all the migrations.
 ./drush.sh cr
 ./drush.sh migrate:rollback --tag="digitalgov"
 ./drush.sh migrate:import --tag="digitalgov"
 
-# 3. Clean up migrated content (shortcodes, media links, emoji).
+# 4. Clean up migrated content (shortcodes, media links, emoji).
 ./drush.sh digitalgov:update-nodes
 ./drush.sh digitalgov:update-paragraphs
 
