e54068a9aacee94144b33cc9f609a5c53472fcfe
Matt Poole <mattsqd@users.noreply.github.com>
2025-03-20 15:05:35 -0400

    DIGITAL-467: Make this script work in remote.

--- migrate-all.sh
+++ migrate-all.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-set -x
+set -xe
+
+DRUSH_BIN="./drush.sh"
+[ -n "$VCAP_APPLICATION" ] && DRUSH_BIN="drush"
 
 # Runs all the migration steps in the expected order
 
 # 1. Remove content created by default content that will be imported by the
 # scripts.
-./drush.sh rcbm
+${DRUSH_BIN} rcbm
 
 # 2. Build the feed for s3files that are directly linked in markdown,
 #    not with the asset short code
-./drush.sh digitalgov:s3feed > web/sites/default/files/s3files.json
+${DRUSH_BIN} digitalgov:s3feed > web/sites/default/files/s3files.json
 
 # 3. Run all the migrations.
-./drush.sh cr
-./drush.sh migrate:rollback --tag="digitalgov"
-./drush.sh migrate:import --tag="digitalgov"
+${DRUSH_BIN} cr
+${DRUSH_BIN} migrate:rollback --tag="digitalgov"
+${DRUSH_BIN} migrate:import --tag="digitalgov"
 
 # 4. Clean up migrated content (shortcodes, media links, emoji).
-./drush.sh digitalgov:update-nodes
-./drush.sh digitalgov:update-paragraphs
+${DRUSH_BIN} digitalgov:update-nodes
+${DRUSH_BIN} digitalgov:update-paragraphs
 
 #rm ./web/sites/default/files/s3files.json
